f8d6815acec42610658339602cdf6294686705b0
Rekha C <78464358+Rekha-chittibabu@users.noreply.github.com>
2025-11-29 12:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (12.36%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -13,28 +13,29 @@ class Solution {
         // 1 -> 2 -> 4
         // 1 -> 3 -> 4
 
-        ListNode l1 = list1;
-        ListNode l2 = list2;
-        ListNode dummy = new ListNode(-1);  // Like a placeholder head
-        ListNode current = dummy;           // Pointer to build result
+       ListNode l1 = list1;
+       ListNode l2 = list2;;
 
+       ListNode dummy = new ListNode(-1);
+       ListNode current = dummy;
 
-        while(l1!=null && l2!=null){
-            if(l1.val < l2.val){
-                current.next = l1;
-                l1 = l1.next;
-            }
-            else{
-                current.next = l2;
-                l2 = l2.next;
-            }
-            current = current.next;
-        }
+    while(l1!=null && l2!=null){
+       if(l1.val < l2.val){
+            current.next = l1;
+            l1=l1.next;
+       }
+       else{
+            current.next = l2;
+            l2=l2.next;
+       }
 
-         // Attach remaining nodes
-        if (l1 != null) current.next = l1;
-        else current.next = l2;
-
-        return dummy.next; // Return head of merged list
+       current=current.next;
     }
+    if(l1!=null)
+        current.next = l1;
+    else current.next = l2;
+
+    return dummy.next;
+    }
+    
 }
