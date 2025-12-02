88645187452636c8301366c75085fc0cdde919b7
Rekha C <78464358+Rekha-chittibabu@users.noreply.github.com>
2025-12-02 21:06:36 +0530

    Time: 34 ms (29.32%) | Memory: 94.2 MB (33.98%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -9,21 +9,24 @@ Inner while loop → O(n) in total across all runs (not per element)
 
 So total = O(n + n + n) = O(n) */
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0) return 0;
-        Set<Integer> numSet = new HashSet<>();
-        for (int num : nums) {
-            numSet.add(num);
+        int maxLen = 0;
+        int n = nums.length;
+        if(n == 0) return 0;
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
         }
-        int maxLen = Integer.MIN_VALUE;
-        for(int num : numSet){
-            if (!numSet.contains(num-1)){ // this num is start of a sequence
-                int len = 1;
-                while(numSet.contains(num+len)){
-                    len+=1;                 
+
+        for(int num : set){
+            int len = 0;
+            if(!set.contains(num-1)){
+                while(set.contains(num++)){
+                    len++;
                 }
                 maxLen = Math.max(maxLen,len);
             }
         }
-        return maxLen;
+    return maxLen;
     }
 }
