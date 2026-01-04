9aa73c42357728ad1a326e8752aed5cb8ac2f666
Rekha C <78464358+Rekha-chittibabu@users.noreply.github.com>
2026-01-04 11:21:49 +0530

    Time: 4 ms (72.80%) | Memory: 45.1 MB (68.09%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -10,22 +10,24 @@ Once we find a bigger number, we resolve all smaller numbers in the stack.*/
         Stack<Integer> stack = new Stack<>();   
         Map<Integer,Integer> nge = new HashMap<>();
 
+        // Build Next greater element map
+
         for(int num : nums2){
             while(!stack.isEmpty() && num > stack.peek()){
-                nge.put(stack.pop(),num);
+                nge.put(stack.pop(),num); // peek value is nge for num
             }
             stack.push(num);
         }
 
+        // for rem ele in stack , nge is -1
         while(!stack.isEmpty()){
             nge.put(stack.pop(),-1);
         }
 
         int[] result = new int[n];
-        int k = 0;
-        for(int ele : nums1){
-            result[k] = nge.get(ele);
-            k++;
+
+        for(int i=0;i<n;i++){
+            result[i]=nge.get(nums1[i]);
         }
     return result;
     }
