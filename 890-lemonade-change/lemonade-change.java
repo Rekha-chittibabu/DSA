80dd0f3098de850ba2c1b2eec970a21289ef4f1f
Rekha C <78464358+Rekha-chittibabu@users.noreply.github.com>
2026-01-19 19:28:32 +0530

    Time: 2 ms (99.09%) | Memory: 72.4 MB (54.69%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -2,31 +2,33 @@ class Solution {
     public boolean lemonadeChange(int[] bills) {
         int five = 0, ten = 0 ;
 
+// Those who are wondering where is greedy approach in this question: 
+// suppose a customer pays 20 Rs then we first try to give him change by giving 10 Rs and 5 Rs  and if are unable to do then we given three 5 Rs change , in this way saving 5 Rs for future customers.
         for(int bill : bills){
-            if(bill == 5)
+            if(bill == 5){
                 five++;
-            else if(bill == 10){
-                if(five>=1){
-                    five--;
-                    ten++;
+            }
+            else if (bill == 10){
+                ten++;
+                if(five < 1){
+                    return false;
                 }
                 else{
-                    return false;
+                    // give 5 as change
+                    five--;
                 }
             }
 
             else{
                 if(ten >= 1 && five >= 1){
-                    ten--;
-                    five--;
+                    ten--; five--;
                 }
-                else if (five>=3)
-                    five = five - 3;
-                else{
-                    return false;
-                }
+                    
+                else if(five < 3) return false;
+                else five = five - 3;
             }
-        }
+
+    }
     return true;
     }
 }
