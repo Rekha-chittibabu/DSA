f72c468e7a63d69cc145f5e07d0b0e579dce9ede
Rekha C <78464358+Rekha-chittibabu@users.noreply.github.com>
2026-01-20 08:33:46 +0530

    Time: 1 ms (99.52%) | Memory: 47.5 MB (26.99%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,23 +1,28 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int jumps = 0;
-        int l = 0;
-        int r = 0;
-        int farthest = 0;
 
-        while( r < nums.length - 1){
-            farthest = 0;
-            for( int i = l;i<=r;i++){
-                farthest = Math.max(farthest,i + nums[i]);
+        int jumps = 0;      // number of jumps taken (BFS levels)
+        int l = 0;          // left boundary of current BFS level
+        int r = 0;          // right boundary of current BFS level
+
+        // Continue until the current level can reach the last index
+        while (r < n - 1) {
+
+            int farthest = 0;   // farthest index reachable from this level
+
+            // Explore all indices in the current BFS level [l..r]
+            for (int i = l; i <= r; i++) {
+                // From index i, we can reach up to i + nums[i]
+                farthest = Math.max(farthest, i + nums[i]);
             }
-            l = r + 1;
-            r = farthest;
-            jumps++;
+
+            // Move to the next BFS level
+            l = r + 1;          // next level starts after current r
+            r = farthest;       // next level ends at farthest reachable index
+            jumps++;            // one jump completed (one BFS level)
         }
 
-        
-    return jumps;
-        
+        return jumps;
     }
 }
